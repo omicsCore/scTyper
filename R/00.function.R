91cf94521a738b8c37f32c68f4886e4d0580ee33
hyeiny119 <hyeiny119@naver.com>
2020-06-23 14:18:40 +0900

    second commit

---
+++ R/00.function.R
@@ -0,0 +1,267 @@
+#' @title make.color.sample
+#' @description make ramdom colors
+#' @usage make.color.sample(n)
+#' @param n the number  of color sample, integer
+#' @param col.label color label, character
+#' @export
+make.color.set <- function(n, col.label){
+  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  cl=sample(col_vector, n)
+  names(cl)=col.label
+  return(cl)
+}
+
+
+#' @title get.markerList
+#' @usage get.markerList(wd, marker)
+#' @description get markerList from scTyper databse
+#' @param wd working directory
+#' @param marker Signature_list or Signature name of scTyper db or User-defined list of marker genes
+#' @return marker list
+#' @export
+get.markerList=function(marker="Puram.2017.HNSCC.TME"){
+  if(class(marker)=="character"){
+    CellMarker=scTyper::CellMarker
+    sigTyper.db=scTyper::sigTyper.db
+    markerDB=c(CellMarker, sigTyper.db)
+
+    markerList=list()
+    for(i in 1:length(marker)){
+      varname=strsplit(marker, split=":")[[i]][1]
+      one_markerlist=markerDB[[varname]]
+      if(!is.na(strsplit(marker, split=":")[[i]][3])) {one_markerlist=one_markerlist[strsplit(marker, split=":")[[i]][3]]}
+      markerList=c(markerList, one_markerlist)
+    }
+
+    ##remove overlapped gene
+    if(any(as.vector(table(as.character(unlist(markerList))))>1)==TRUE){
+      df=data.frame(table(as.character(unlist(markerList))))
+      df_freq1gene=df[df$Freq==1,]
+      lstdiff_gene=df_freq1gene$Var1
+      markerList.o=markerList
+      markerList=sapply(1:length(markerList), function(i) markerList[[i]]=intersect(markerList.o[[i]], lstdiff_gene) )
+
+      names(markerList)=names(markerList.o)
+    }
+
+    markerList_name=unique(names(markerList))
+    for(i in 1:length(unique(names(markerList)))){
+      markerList_markers=unique(as.character(unlist(markerList[names(markerList) == markerList_name[i]])))
+      markerList[names(markerList) == markerList_name[i]]=NULL
+      markerList[[markerList_name[i]]]=markerList_markers
+    }
+
+  }else if(class(marker)=="list"){
+    markerList=marker
+  }else{
+    stop("### Error :: Please write upper case letter and Class of marker is not list or character###")
+  }
+  return(markerList)
+}
+
+#' @title list2matrix
+#' @usage list2matrix(List)
+#' @description convert list to matrix
+#' @param List list
+#' @export
+list2matrix<-function(List){
+  maxn  =max(unlist(lapply(List, length)))
+  for (i in 1:length(List))  length(List[[i]])=maxn
+  res=NULL
+  for (i in 1:length(List)){ res=cbind(res, as.character(List[[i]]))}
+  colnames(res)=names(List)
+  res[which(is.na(res))]=""
+  return(res)
+}
+
+
+#' @title preRanked.GSEA
+#' @usage preRanked.GSEA(expr, SIGDB, weighted.score.type = 0, correl.vector = NULL, n.cutoff=1,mc.cores=1)
+#' @description make GSEA data
+#' @param expr expression metrix
+#' @param SIGDB gene signature list
+#' @param weighted.score.type Type of weight score
+#' @param correl.vector correlation vector
+#' @param n.cutoff number of cutoff
+#' @param mc.cores The number of cores to use. Must be at least one(default=1), and parallelization requires at least two cores.
+#' @export
+preRanked.GSEA<-function (expr, SIGDB, weighted.score.type = 0, correl.vector = NULL, n.cutoff=1, mc.cores=1){
+  #preprocess(expr, method="c", cen.method="g")
+  geneList = lapply(SIGDB, function(a) intersect(a, rownames(expr)))
+  gene.set.order = lapply(geneList, function(a) match(a, rownames(expr)))
+  gene.set.order=gene.set.order[lapply(gene.set.order, length)>=n.cutoff]
+  ES = mclapply(as.list(1:ncol(expr)), function(a) as.numeric(lapply(gene.set.order,  function(b) as.numeric(GSEA.EnrichmentScore2(order(-expr[, a]), b, weighted.score.type = weighted.score.type, correl.vector = correl.vector)))), mc.cores=mc.cores)
+  ES = apply(list2matrix(ES), 2, as.numeric)
+  rownames(ES) = names(gene.set.order)
+  colnames(ES) = colnames(expr)
+  return(ES)
+}
+
+
+#' @title GSEA.EnrichmentScore2
+#' @usage GSEA.EnrichmentScore2(gene.list, gene.set, weighted.score.type = 1, correl.vector = NULL)
+#' @description Run GSEA(Gene Set Enrichment Analysis)
+#' @param gene.list gene signature list
+#' @param gene.set gene set
+#' @param weighted.score.type Type of weighted score
+#' @param correl.vector correlation vector
+#' @export
+GSEA.EnrichmentScore2<-function (gene.list, gene.set, weighted.score.type = 1, correl.vector = NULL) {
+  N <- length(gene.list)
+  Nh <- length(gene.set)
+  Nm <- N - Nh
+  loc.vector <- vector(length = N, mode = "numeric")
+  peak.res.vector <- vector(length = Nh, mode = "numeric")
+  valley.res.vector <- vector(length = Nh, mode = "numeric")
+  tag.correl.vector <- vector(length = Nh, mode = "numeric")
+  tag.diff.vector <- vector(length = Nh, mode = "numeric")
+  tag.loc.vector <- vector(length = Nh, mode = "numeric")
+  loc.vector[gene.list] <- seq(1, N)
+  tag.loc.vector <- loc.vector[gene.set]
+  tag.loc.vector <- sort(tag.loc.vector, decreasing = F)
+  if (weighted.score.type == 0) {
+    tag.correl.vector <- rep(1, Nh)
+  }
+  else if (weighted.score.type == 1) {
+    tag.correl.vector <- correl.vector[tag.loc.vector]
+    tag.correl.vector <- abs(tag.correl.vector)
+  }
+  else if (weighted.score.type == 2) {
+    tag.correl.vector <- correl.vector[tag.loc.vector] *
+      correl.vector[tag.loc.vector]
+    tag.correl.vector <- abs(tag.correl.vector)
+  }
+  else {
+    tag.correl.vector <- correl.vector[tag.loc.vector]^weighted.score.type
+    tag.correl.vector <- abs(tag.correl.vector)
+  }
+  norm.tag <- 1/sum(tag.correl.vector)
+  tag.correl.vector <- tag.correl.vector * norm.tag
+  norm.no.tag <- 1/Nm
+  tag.diff.vector[1] <- (tag.loc.vector[1] - 1)
+  tag.diff.vector[2:Nh] <- tag.loc.vector[2:Nh] - tag.loc.vector[1:(Nh -
+                                                                      1)] - 1
+  tag.diff.vector <- tag.diff.vector * norm.no.tag
+  peak.res.vector <- cumsum(tag.correl.vector - tag.diff.vector)
+  valley.res.vector <- peak.res.vector - tag.correl.vector
+  max.ES <- max(peak.res.vector)
+  min.ES <- min(valley.res.vector)
+  ES <- signif(ifelse(max.ES > -min.ES, max.ES, min.ES), digits = 5)
+  return(list(ES = ES))
+}
+
+#' @title perm.ttest
+#' @description Run permutation T-test
+#' @usage perm.ttest(eset, g.st,  level=NULL, t.test=F, permp=T, permp.exact=NULL, ordered=T, mc.cores=1,...)
+#' @param eset expression set
+#' @param g.st group subset
+#' @param levels confidence level of the interval.
+#' @param permp bool, defalut TRUE. Calculating permuted T test p-values or not
+#' @param ordered order bool, default FALSE. Sort descending vs. ascending
+#' @param mc.cores The number of cores to use. Must be at least one(default=1), and parallelization requires at least two cores.
+#' @details Performs sample t-tests on vectors of data.
+#' @return t test result
+#' @import parallel
+#' @export
+perm.ttest =function(eset, g.st,  level=NULL, t.test=FALSE, permp=TRUE, ordered=T, mc.cores=1,...){
+  if(inherits(eset, "ExpressionSet"))   expr=exprs(eset) else    expr=eset
+
+  if(ncol(expr)!=length(g.st))  cat("class labels has a different length")
+  if(!inherits(g.st, "factor")) g.st=factor(g.st)
+  if(!is.null(level)) g.st=factor(g.st, level=level)
+  res=NULL
+  if(t.test){
+    message("Calculating T test p-values")
+    if(mc.cores>1){
+      if(Sys.info()[['sysname']]=="Windows"){
+        res=mclapply(1:nrow(expr), function(a) try(t.test(as.numeric(expr[a,])~g.st), silent=T),mc.cores=mc.cores,expr=expr,g.st=g.st, packageToLoad=c("stat","perm"))
+      }else{
+        res=mclapply(1:nrow(expr), function(a) try(t.test(as.numeric(expr[a,])~g.st), silent=T),mc.cores=mc.cores,...=...)
+      }
+    }
+    if(mc.cores==1) res=lapply(1:nrow(expr), function(a) try(t.test(as.numeric(expr[a,])~g.st), silent=T))
+
+    tval=as.numeric(sapply(res, function(a) try(a$stat, silent=T)))
+    test.p=as.numeric(sapply(res, function(a) try(a$p.val, silent=T)))
+    res=data.frame(t.stat=(tval), ttest.p=test.p)
+    rownames(res)=rownames(expr)
+  }
+  if(permp)  {
+
+    message("Calculating permuted T test p-values")
+    if(mc.cores>1){
+      if(Sys.info()[['sysname']]=="Windows") {
+        res$perm.p=as.numeric(mclapply(1:nrow(expr), function(a) try(permTS(as.numeric(expr[a,]) ~ g.st)$p.value, silent=T),mc.cores=mc.cores,cluster.export=F, expr=expr,g.st=g.st, packageToLoad=c("stat","perm")))
+      }else{
+        res$perm.p=as.numeric(parallel::mclapply(1:nrow(expr), function(a) try(permTS(as.numeric(expr[a,]) ~ g.st)$p.value, silent=T),mc.cores=mc.cores))
+      }
+    }
+    if(mc.cores==1) res$perm.p=as.numeric(lapply(1:nrow(expr), function(a) try(permTS(as.numeric(expr[a,]) ~ g.st)$p.value, silent=T)))
+    res$FDR = p.adjust(res$perm.p, "BH")
+  }
+
+  class.mean=sapply(levels(g.st), function(a) rowMeans(expr[,which(g.st==a)], na.rm=T))
+  colnames(class.mean) = paste(colnames(class.mean), "(mean)")
+  fc=as.matrix(class.mean[,1]-class.mean[,2])
+  res=cbind(as.data.frame(res), class.mean, fc)
+
+  if(ordered)  res=res[order(-fc),]
+  return(res)
+}
+
+#' @title df2gr
+#' @description A wrapper function to make dataframe to GRange
+#' @usage df2gr(df, seqnames, start, end, strand)
+#' @param df dataframe
+#' @param seqnames A character vector of recognized names for the column in df that contains the chromosome name (sequence name) associated with each genomic range.
+#' @param start A character vector of recognized names for the column in df that contains the start positions of the genomic ranges.
+#' @param end A character vector of recognized names for the column in df that contains the end positions of the genomic ranges.
+#' @param strand A character vector of recognized names for the column in df that contains the strand associated with each genomic range.
+#' @details the workhorse behind the coercion method from data.frame to GRanges.
+#' @return GRanges object
+#' @export
+df2gr=function(df, seqnames, start, end, strand){
+  meta.st=setdiff(names(df),c(seqnames, start, end,strand))
+  start=as.numeric(df[,start])
+  end=as.numeric(df[,end])
+  strand=as.character(df[,strand])
+  seqnames=update.seqnames(df[,seqnames])
+  if(invalid(strand)) strand=NULL
+  GRanges(seqname=seqnames,strand, ranges=IRanges(start,end), df[,meta.st])
+}
+
+
+#' @title update.seqnames
+#' @description updata the column in df that contains the chromosome name (sequence name)
+#' @usage update.seqnames(seqnames)
+#' @param seqnames A character vector of recognized names for the column in df that contains the chromosome name (sequence name) associated with each genomic range.
+#' @return sequence name
+#' @export
+update.seqnames=function(seqnames){
+  seqnames=as.character(seqnames)
+  seqnames=sub("chr", "",seqnames, ignore.case=T)
+  seqnames=sub("23", "X", seqnames, ignore.case=T)
+  seqnames=sub("24", "Y", seqnames, ignore.case=T)
+  seqnames=paste0("chr", seqnames)
+  seqnames
+}
+
+
+#' @title invalids
+#' @description Test if a value is missing, empty, or contains only NA or NULL values.
+#' @usage invalid(x)
+#' @param x value to be tested
+#' @return Bool
+#' @export
+invalid= function (x)
+{
+  if (missing(x) || is.null(x) || length(x) == 0)
+    return(TRUE)
+  if (is.list(x))
+    return(all(sapply(x, invalid)))
+  else if (is.vector(x))
+    return(all(is.na(x)))
+  else return(FALSE)
+}
+
---
+++ R/scTyper.R
@@ -0,0 +1,189 @@
+#' @title scTyper
+#' @description Run scTyper pipeline
+#' @usage scTyper()
+#' @param wd Working directory
+#' @param output.name Output directory name
+#' @param pheno.fn Phenotype file path
+#' @param qc  Whether to execute FASTQC (default=FALSE)
+#' @param run.cellranger whether to excute cellranger count (default=FALSE)
+#' @param norm.seurat whether to normalize seurat object (default=FALSE)
+#' @param cell.typing.method cell typing method, c("NTP", "ES", "Average"), (default = "NTP")
+#' @param level Indicate the cell assignment level (cell or cluster)
+#' @param run.inferCNV Indicate whether ‘malignant cell typing by inferCNV process run
+#' @param proj.name Project name
+#' @param fastqc.path FastQC program path
+#' @param fastq.dir FastQC output directory
+#' @param fq1.idx Index of the FASTQ file (Read 1)
+#' @param fq2.idx Index of the FASTQ file (Read 2)
+#' @param cellranger.path Cell Ranger program path
+#' @param cellranger.ref.dir Directory of Cell Ranger reference file
+#' @param percent.min.cells Cutoff to filter features containing minimum percent of cells
+#' @param min.features Cutoff to filter cells containing minimum number of features
+#' @param percent.mt Cutoff for filtering cells that have >n percent mitochondrial counts
+#' @param vars.to.regress Variables to regress out
+#' @param dims A vector of the dimensions to use in construction of the SNN graph.
+#' @param resolution Value of the resolution parameter, use a value above (below) 1.0 if you want to obtain a larger (smaller) number of communities.
+#' @param seurat.object Seurat object, if user don't have pre-processed seurat object, user have to insert reference seurat object
+#' @param slot Data type of Seurat object, c("scale.data", "count.data", "data")
+#' @param marker Cell markers to use cell typing, Character or List (Signature names or Study names or User defined gene list)
+#' @param assay Assay of Seurat object
+#' @param NTP.g.filter.method Method to filter genes in NTP
+#' @param NTP.gene.filter.cutoff Cutoff to filter genes of in NTP
+#' @param NTP.distance NTP distance method, a character, either c("correlation" or "cosine").
+#' @param NTP.norm.method NTP normalization method, either c("none", "row.std")
+#' @param gene.ref.gtf Path of GTF file including genomic location for genes
+#' @param feature.to.test Column header name of the meta data in Seurat object (select the cell groups for T.test) either "tissue.type" or "cell.type"
+#' @param cells.test_excluded A value indicates the cells to be excluded in T.test
+#' @param cells.test_reference A value indicates the cells to use as be excluded in T.test
+#' @param fc.cutoff Cutoff of fold change
+#' @param cutoff.gene.cluster A cutoff P-value for filtering out the gene clusters (calculated from GO analysis)
+#' @param malignant.cell.type Cell type to assign malignant cell
+#' @param report.mode Generate report file
+#' @param mc.cores The number of cores to use. Must be at least one(default=1), and parallelization requires at least two cores.
+#' @return Seurat object
+#' @export
+scTyper<- function(wd=getwd(),
+                   output.name = "test.result",
+                   pheno.fn,
+                   qc = FALSE,
+                   run.cellranger=FALSE,
+                   norm.seurat=FALSE,
+                   cell.typing.method=c("NTP", "ES", "Average"),
+                   level=c("cell","cluster"),
+                   run.inferCNV=TRUE,
+                   proj.name = "scTyper",
+
+                   fastqc.path=NULL,
+                   fastq.dir=NULL,
+                   fq1.idx="_R1_001.fastq",
+                   fq2.idx="_R2_001.fastq",
+
+                   cellranger.path=NULL,
+                   cellranger.ref.dir=NULL,
+
+                   percent.min.cells=0.1,
+                   min.features=200,
+                   percent.mt=10,
+                   vars.to.regress=c("nCount_RNA", "percent.mt"),
+                   dims=1:10,
+                   resolution=2,
+
+                   seurat.object=NULL,
+                   slot=c("scale.data", "count.data", "data"),
+                   marker="Puram.2017.HNSCC.TME",
+                   assay="RNA",
+                   NTP.g.filter.method=c("sd", "mad", "none"),
+                   NTP.gene.filter.cutoff=0.3,
+                   NTP.distance=c("cosine","correlation"),
+                   NTP.norm.method=c("none", "row.std"),
+
+                   gene.ref.gtf=NULL,
+                   feature.to.test = c("tissue.type", "cell.type"),
+                   cells.test_excluded="Epithelial",
+                   cells.test_reference = "immune",
+                   fc.cutoff=0.05,
+                   cutoff.gene.cluster=0.05,
+                   malignant.cell.type="Epithelial",
+                   report.mode=TRUE,
+                   mc.cores = 1){
+
+  start_time <- Sys.time()
+  cell.typing.method=match.arg(cell.typing.method)
+  level=match.arg(level)
+  slot=match.arg(slot)
+  NTP.gene.filter.method=match.arg(NTP.g.filter.method)
+  NTP.distance=match.arg(NTP.distance)
+  NTP.norm.method=match.arg(NTP.norm.method)
+  feature.to.test=match.arg(feature.to.test)
+
+  # Loading package
+  library(fastqcr)
+  library(Seurat)
+  library(infercnv)
+  library(base)
+  library(utils)
+  library(gProfileR)
+  library(S4Vectors)
+  library(rmarkdown)
+  library(e1071)
+  library(parallel)
+  library(perm)
+  library(Biobase)
+  library(reshape2)
+  library(IRanges)
+  library(limma)
+  library(ggplot2)
+  library(grid)
+  library(gridExtra)
+  library(GenomicRanges)
+  library(ComplexHeatmap)
+  library(circlize)
+  library(magrittr)
+  library(knitr)
+  library(kableExtra)
+  library(RColorBrewer)
+  library(pander)
+  library(png)
+  library(grDevices)
+  library(colorspace)
+
+
+  # default outdirs
+  qc.dir <- file.path(wd, output.name, "00_qc")
+  count.dir <- file.path(wd, output.name,　"01_count")
+  ntp.dir <- file.path(wd, output.name,　"02_NTP")
+  inferCNV.dir  <- file.path(wd, output.name,　"03_inferCNV")
+  rda.dir <- file.path(wd, output.name,　"data")
+
+
+
+  #######################
+  ### start scTyper
+  dir.create(file.path(wd, output.name), showWarnings = F)
+  dir.create(file.path(rda.dir), showWarnings = F)
+
+  pheno.df = read.csv(pheno.fn, header = T, sep = ",")
+  sample.name = as.character(pheno.df$Sample_ID)
+
+  if(qc==TRUE){
+    fastqc.outs = fastqc(fastqc.path, fastq.dir=fastq.dir, sample.name, fq1.idx=fq1.idx, fq2.idx=fq2.idx, output.dir=qc.dir, run.cmd=TRUE, mc.cores=mc.cores)
+  }
+
+  if(run.cellranger==TRUE){
+    count.out.dirs = CellrangerCount(cellranger.path, fastq.dir=fastq.dir, output.dir=count.dir, cellranger.ref.dir=cellranger.ref.dir, sample.name=sample.name, run.cmd=TRUE, mc.cores=mc.cores)
+    setwd(wd)
+    metrics_summary = make.stat_summary(count.dir = count.dir, sample.name = sample.name, pheno.df=pheno.df, output.dir = count.dir)
+  }
+
+  if(norm.seurat==TRUE){
+    seurat = run.seurat.process(count.dir = count.dir, rda.dir = rda.dir, project = proj.name, sample.name = sample.name, metrics_summary,
+                                percent.min.cells=percent.min.cells, min.features=min.features, percent.mt=percent.mt, vars.to.regress=vars.to.regress, dims=dims,
+                                resolution=resolution)
+  }
+
+  if(class(seurat.object)=="Seurat") seurat=seurat.object
+
+  ##cell typing
+  seurat=cell.typing.seurat(seurat=seurat, marker=marker, wd=wd, slot=slot, cell.typing.method=cell.typing.method, level=level, assay=assay, ntp.dir=ntp.dir, rda.dir=rda.dir, NTP.g.filter.method=NTP.g.filter.method, NTP.gene.filter.cutoff=NTP.gene.filter.cutoff, NTP.distance=NTP.distance, NTP.norm.method=NTP.norm.method, mc.cores=mc.cores)
+
+  seurat@meta.data$tissue.type = pheno.df[match(seurat@meta.data$sample.name, pheno.df[,"Sample_ID"]),"TissueType"]
+
+  if(run.inferCNV==TRUE){
+    fdata = make.seurat.fdata(seurat, rda.dir, gene.ref.gtf = gene.ref.gtf)
+
+    seurat = run.inferCNV(seurat = seurat, assay=assay, fdata, pheno_info = pheno.df, output.dir = inferCNV.dir, rda.dir = rda.dir, feature.to.test=feature.to.test, cells.test_reference=cells.test_reference, cells.test_excluded=cells.test_excluded, fc.cutoff=fc.cutoff, cutoff.gene.cluster=cutoff.gene.cluster, mc.cores=mc.cores)
+
+    seurat = malignant.cellTyper(seurat = seurat, rda.dir = rda.dir, cells.test_reference=cells.test_reference, malignant.cell.type=malignant.cell.type)
+
+  }
+  end_time <- Sys.time()
+  runtime = format(end_time-start_time, format="%H")
+  #report
+  envList=as.list(environment())
+  if(report.mode) {report(envList, qc.dir, output.dir=file.path(wd, output.name))}
+
+  return(seurat)
+}
+
+
+
